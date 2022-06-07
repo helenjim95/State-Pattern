ed8ee4d0b0f9fc854e25c160218a779f1102227f
Lucas Welscher <lucas.welscher@tum.de>
2022-06-07 18:52:28 +0200

    Exercise-Template pushed by Artemis

---
+++ src/de/tum/in/ase/eist/Controller.java
@@ -0,0 +1,23 @@
+package de.tum.in.ase.eist;
+
+public class Controller {
+
+	// TODO: implement attributes
+	// TODO: implement methods
+
+	public Controller(int currentFloor) {
+	}
+
+	public void request(int floor) {
+		// TODO
+	}
+
+	public void signal(int floor) {
+		// TODO
+	}
+
+	public ElevatorState getState() {
+		// TODO
+		return null;
+	}
+}
