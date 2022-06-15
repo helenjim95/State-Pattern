a20a7258edb5d102e1fafb3bb13a9f61ddd41447
helenjim49 <helenjim49@gmail.com>
2022-06-15 15:10:44 +0200

    init

--- src/de/tum/in/ase/eist/Controller.java
+++ src/de/tum/in/ase/eist/Controller.java
@@ -1,23 +1,159 @@
 package de.tum.in.ase.eist;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 public class Controller {
 
-	// TODO: implement attributes
-	// TODO: implement methods
+	private int currentFloor;
+	private List<Integer> pool;
+	private ElevatorState state;
 
 	public Controller(int currentFloor) {
+		this.currentFloor = currentFloor;
+		pool = new List<Integer>() {
+			@Override
+			public int size() {
+				return 0;
+			}
+
+			@Override
+			public boolean isEmpty() {
+				return false;
+			}
+
+			@Override
+			public boolean contains(Object o) {
+				return false;
+			}
+
+			@Override
+			public Iterator<Integer> iterator() {
+				return null;
+			}
+
+			@Override
+			public Object[] toArray() {
+				return new Object[0];
+			}
+
+			@Override
+			public <T> T[] toArray(T[] a) {
+				return null;
+			}
+
+			@Override
+			public boolean add(Integer integer) {
+				return false;
+			}
+
+			@Override
+			public boolean remove(Object o) {
+				return false;
+			}
+
+			@Override
+			public boolean containsAll(Collection<?> c) {
+				return false;
+			}
+
+			@Override
+			public boolean addAll(Collection<? extends Integer> c) {
+				return false;
+			}
+
+			@Override
+			public boolean addAll(int index, Collection<? extends Integer> c) {
+				return false;
+			}
+
+			@Override
+			public boolean removeAll(Collection<?> c) {
+				return false;
+			}
+
+			@Override
+			public boolean retainAll(Collection<?> c) {
+				return false;
+			}
+
+			@Override
+			public void clear() {
+
+			}
+
+			@Override
+			public Integer get(int index) {
+				return null;
+			}
+
+			@Override
+			public Integer set(int index, Integer element) {
+				return null;
+			}
+
+			@Override
+			public void add(int index, Integer element) {
+
+			}
+
+			@Override
+			public Integer remove(int index) {
+				return null;
+			}
+
+			@Override
+			public int indexOf(Object o) {
+				return 0;
+			}
+
+			@Override
+			public int lastIndexOf(Object o) {
+				return 0;
+			}
+
+			@Override
+			public ListIterator<Integer> listIterator() {
+				return null;
+			}
+
+			@Override
+			public ListIterator<Integer> listIterator(int index) {
+				return null;
+			}
+
+			@Override
+			public List<Integer> subList(int fromIndex, int toIndex) {
+				return null;
+			}
+		}
 	}
 
 	public void request(int floor) {
-		// TODO
+		pool.add(floor);
 	}
 
 	public void signal(int floor) {
-		// TODO
+		pool.remove(floor);
 	}
 
 	public ElevatorState getState() {
-		// TODO
-		return null;
+		return state.getName();
 	}
+
+	public int getNexthop() {
+
+		if (!pool.isEmpty()) {
+			return pool.get(0);
+		} else {
+			return -1;
+		}
+	}
+
+	public int getCurrentFloor() {
+		return currentFloor;
+	}
+
 }
---
+++ src/de/tum/in/ase/eist/moving.java
@@ -0,0 +1,18 @@
+package de.tum.in.ase.eist;
+
+public class moving extends ElevatorState {
+    @Override
+    public void request(int floor, Controller controller) {
+
+    }
+
+    @Override
+    public void signal(int floor, Controller controller) {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
